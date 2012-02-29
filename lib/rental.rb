702085b3723aefb73976ee93ea8e58dfd535eae9
sinclair bain <github.stuff@gmail.com>
2012-02-29 07:47:16 -0500

    fix typo

--- lib/rental.rb
+++ lib/rental.rb
@@ -3,7 +3,7 @@ class Rental
   attr_reader :movie, :days_rented
 
   def initialize(movie, days_rented)
-    @movie, @days_rentend = movie, days_rented.to_i()
+    @movie, @days_rented = movie, days_rented.to_i()
   end
 
 end
